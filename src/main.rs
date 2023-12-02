e71b730b847be214f79c738fcde4ab5dca2a6050
Matt Lautzenheiser <mlautz@headstorm.com>
2023-12-02 14:03:54 -0700

    Day 2 - Part 2 solution.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod day_1_part_1_solution;
 mod day_1_part_2_solution;
 mod day_2_part_1_solution;
+mod day_2_part_2_solution;
 
 use std::env;
 use crate::day_1_part_1_solution::day_one_part_one_solution;
 use crate::day_1_part_2_solution::day_one_part_two_solution;
 use crate::day_2_part_1_solution::day_two_part_one_solution;
+use crate::day_2_part_2_solution::day_two_part_two_solution;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -25,6 +27,7 @@ fn main() {
         "day1part1"=> day_one_part_one_solution(),
         "day1part2"=> day_one_part_two_solution(),
         "day2part1"=> day_two_part_one_solution(),
+        "day2part2"=> day_two_part_two_solution(),
         _=>println!("No match for day provided.")
     }
 }
