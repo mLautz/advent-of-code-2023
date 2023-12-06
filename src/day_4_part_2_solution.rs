4212a0c278443313108ea70c1efe35347d190f87
Matt Lautzenheiser <mlautz@headstorm.com>
2023-12-06 11:55:49 -0700

    Day 4 - Part 2 Solution.

---
+++ src/day_4_part_2_solution.rs
@@ -0,0 +1,74 @@
+use std::fs;
+use regex::Regex;
+
+pub fn day_four_part_two_solution() {
+    println!("Day 4 - Part 1");
+
+    let contents = fs::read_to_string("./inputs/day-4-input").unwrap();
+    let line_mapping: Vec<u32> = process_cards(&contents);
+
+    let card_count: Vec<u32> = (0..(contents.lines().count() as u32)).collect();
+    let mut total_card_count: u32 = 0;
+
+    card_count.into_iter().for_each(|card| {
+        total_card_count += evaluate_cards(&line_mapping, card);
+    });
+
+    println!("\n\nFinal Card Sum ====> {}", total_card_count);
+}
+
+fn process_cards(content: &String) -> Vec<u32> {
+    let mut line_mapping: Vec<u32> = Vec::new();
+
+    let mut line_count: u32 = 0;
+    for line in content.lines() {
+        let line_sans_card_num: Vec<&str> = line.split(&[':','|'][..]).collect();
+
+        let winning_num_string = line_sans_card_num.get(1).unwrap();
+        let eligible_num_string = line_sans_card_num.get(2).unwrap();
+
+        let winning_nums = extract_digits(winning_num_string);
+        let eligible_nums = extract_digits(eligible_num_string);
+
+        let mut match_count = 0;
+        winning_nums.iter().for_each(|num| {
+            if eligible_nums.contains(num) {
+                match_count += 1;
+            }
+        });
+
+        line_mapping.push(match_count);
+    }
+
+    return line_mapping;
+}
+
+fn evaluate_cards(line_mapping: &Vec<u32>, current_card: u32) ->  u32{
+    // println!("\n\nEvaluating new line: {}", current_card);
+    let winning_count_lookup: Option<&u32> = line_mapping.get(current_card as usize);
+    let winning_count: u32 = winning_count_lookup.unwrap_or(&0).clone();
+
+    let mut card_count = 1;
+
+    let mut subsequent_card_index = 0;
+    (0..winning_count).collect::<Vec<u32>>().iter().for_each(|num| {
+        subsequent_card_index += 1;
+        card_count += evaluate_cards(line_mapping, current_card + subsequent_card_index);
+    });
+
+    // println!("Returning card count from line {} --> {}", current_card, card_count);
+    return card_count;
+}
+
+fn extract_digits(num_string: &str) -> Vec<u32> {
+    let digit_regex = Regex::new(r"\d+").unwrap();
+
+    let mut nums = Vec::new();
+
+    for (num_match, []) in digit_regex.captures_iter(num_string).map(|cap| cap.extract()) {
+        let parsed_num = num_match.parse::<u32>().unwrap();
+        nums.push(parsed_num);
+    }
+
+    return nums;
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod day_2_part_1_solution;
 mod day_2_part_2_solution;
 mod day_3_part_1_solution;
 mod day_4_part_1_solution;
+mod day_4_part_2_solution;
 
 use std::env;
 use crate::day_1_part_1_solution::day_one_part_one_solution;
@@ -12,6 +13,7 @@ use crate::day_2_part_1_solution::day_two_part_one_solution;
 use crate::day_2_part_2_solution::day_two_part_two_solution;
 use crate::day_3_part_1_solution::day_three_part_one_solution;
 use crate::day_4_part_1_solution::day_four_part_one_solution;
+use crate::day_4_part_2_solution::day_four_part_two_solution;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -34,6 +36,7 @@ fn main() {
         "day2part2"=> day_two_part_two_solution(),
         "day3part1"=> day_three_part_one_solution(),
         "day4part1"=> day_four_part_one_solution(),
+        "day4part2"=> day_four_part_two_solution(),
         _=>println!("No match for day provided.")
     }
 }
